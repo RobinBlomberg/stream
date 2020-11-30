a59f34d5993307ea8d9ccfc748c2bf733bce6a29
Robin <hello@robinblomberg.se>
2020-11-30 23:09:15 +0100

    Add missing Stream constructor typing

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,7 @@
+import { Readable } from 'stream';
+
 export class Stream {
+  constructor(readableStream: Readable);
   async buffer(): Promise<Buffer>;
   async json(): Promise<any>;
   async text(): Promise<string>;
