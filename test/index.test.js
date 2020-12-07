ce83ff21eccdd108fdbfbba190d0c8a0f8d0a497
Robin <hello@robinblomberg.se>
2020-12-08 00:18:02 +0100

    Implement @robinblomberg/test

--- test/index.test.js
+++ test/index.test.js
@@ -1,31 +1,39 @@
-import * as Assert from 'assert';
 import * as Http from 'http';
+import { describe, equal, test } from '@robinblomberg/test';
 import { Stream } from '../lib/index.js';
 
 const PORT = 3000;
 
-const server = Http.createServer(async(req, res) => {
-  const stream = new Stream(req);
-  const data = await stream.text();
-  res.end(data);
-});
+test('@robinblomberg/stream', () => {
+  return describe('json', () => {
+    return new Promise((resolve) => {
+      const server = Http.createServer(async(req, res) => {
+        const stream = new Stream(req);
+        const data = await stream.text();
+        res.end(data);
+      });
 
-server.listen(PORT);
+      server.listen(PORT);
 
-const req = Http.request(`http://localhost:${PORT}`, { method: 'POST' }, async(res) => {
-  const stream = new Stream(res);
-  const data = await stream.json();
+      const req = Http.request(`http://localhost:${PORT}`, { method: 'POST' }, async(res) => {
+        const stream = new Stream(res);
+        const data = await stream.json();
 
-  Assert.deepStrictEqual(
-    data,
-    {
-      name: 42
-    }
-  );
+        equal(
+          data,
+          {
+            name: 42
+          }
+        );
 
-  server.close();
-});
+        server.close();
 
-req.setHeader('Content-Type', 'application/json');
-req.write('{"name":42}');
-req.end();
+        resolve();
+      });
+
+      req.setHeader('Content-Type', 'application/json');
+      req.write('{"name":42}');
+      req.end();
+    });
+  });
+})();
